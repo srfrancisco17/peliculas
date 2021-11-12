95169c5e2c8251b0cdc38a5857666d481e7cb830
Francisco Ortega <srfrancisco17@gmail.com>
2021-11-12 14:48:14 -0500

    #100

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -4,12 +4,48 @@ import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
 
+
   @override
   Widget build(BuildContext context) {
+
+  // TODO Cambiar luego por una instancia de movie
+  final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Home Screen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
+      )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
         ),
       ),
     );
